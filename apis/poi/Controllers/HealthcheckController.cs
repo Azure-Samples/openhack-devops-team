7f83fc345096cc206c81533f4dc1917b44f1777f
Richard Guthrie <rguthrie@Microsoft.com>
2018-05-24 12:13:39 -0700

    Fixed dockerfile and added logging support

--- apis/poi/Controllers/HealthcheckController.cs
+++ apis/poi/Controllers/HealthcheckController.cs
@@ -2,22 +2,32 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 using poi.Models;
 using poi.Data;
+using poi.Utility;
 using Newtonsoft.Json;
 
 
 namespace poi.Controllers
 {
     [Produces("application/json")]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/poi")]
     public class HealthCheckController : ControllerBase
     {
+
+        private readonly ILogger _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
 
         [HttpGet]
         [Produces("application/json", Type = typeof(Healthcheck))]
         public IActionResult Get()
         {
+            _logger.LogInformation(LoggingEvents.Healthcheck, "Healthcheck Requested");
             string response = JsonConvert.SerializeObject(new Healthcheck());
             return Ok(response);
         }
---
+++ apis/poi/Utility/LoggingEvents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace poi.Utility
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class LoggingEvents
+    {
+        public const int Healthcheck = 1000;
+
+        public const int GetAllPOIs = 2001;
+        public const int GetPOIByID = 2002;
+        public const int GetPOIByTripID = 2002;
+
+    }
+
+}
