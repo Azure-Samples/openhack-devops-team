c968ff5ffc249c9ad04b4008e0edf74b8ea08d29
Richard Guthrie <rguthrie@Microsoft.com>
2018-06-12 13:36:39 -0700

    Refactoring to add test support for POI

---
+++ apis/poi/tests/IntegrationTests/POITests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using poi.Controllers;
+using poi.Models;
+using System.Collections.Generic;
+namespace IntegrationTests
+{
+    public class POIIntegrationTests
+    {
+        // private readonly POIController _poi;
+
+        // public POITestController()
+        // {
+        //     _poi = new POIController();
+        //     List<POI> pOIs = _poi.GetAll();
+        //     Assert.True(pOIs.Count > 0);
+
+        // }
+    }
+}
