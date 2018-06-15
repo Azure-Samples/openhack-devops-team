0552e54edd3fab2e108e907a1a01846561c5ec27
Richard Guthrie <rguthriemsft@users.noreply.github.com>
2018-06-15 16:03:57 -0700

    Unit test3 (#22)
    * custom web host for test
    
    * Interim due to host issue
    
    * Working integration test

--- apis/poi/README.md
+++ apis/poi/README.md
@@ -9,10 +9,11 @@
 
 ### Running the unit tests
 
-
-Integration Test
+### Running the Integration Test
 
 ## References
-[Web API](https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-vsc?view=aspnetcore-2.1)
-[Unit Testing](https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test?view=aspnetcore-2.1)
-[Integration Testing](https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-2.1)
+
+* [Web API](https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-vsc?view=aspnetcore-2.1)
+* [Unit Testing](https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test?view=aspnetcore-2.1)
+* [Integration Testing](https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-2.1)
+* [Example - How to Debug .NET Core Xunit Tests](https://github.com/nickolasacosta/dotnetcore-xunit-debugging)
--- apis/poi/tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ apis/poi/tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -3,15 +3,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using poi.Data;
 using IntegrationTests.Utilities;
+using System.IO;
 
 namespace IntegrationTests
 {
     public class CustomWebApplicationFactory<TStartup>
         : WebApplicationFactory<poi.Startup>
     {
+        protected override IWebHostBuilder CreateWebHostBuilder(){
+            //used to read env variables for host/port
+            var configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(configuration)
+                .UseIISIntegration()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
+                .UseStartup<IntegrationTests.Startup>()
+                .UseUrls("http://localhost:8080");
+
+            return host;
+        }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services =>
