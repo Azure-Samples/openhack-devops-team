1b39a5c2a5ea650c4da79d645827808980404738
apraovjr <aprao@microsoft.com>
2018-08-03 15:39:49 -0700

    Add skeleton for JAVA API (#32)
    * Add skeleton for JAVA API
    * Remove swagger version
    * Added working health check controller

---
+++ apis/user-java/src/main/java/io/swagger/api/HealthcheckApiController.java
@@ -0,0 +1,55 @@
+package io.swagger.api;
+
+import io.swagger.model.ErrorResponseDefault;
+import io.swagger.model.Healthcheck;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-08-03T19:26:46.543Z")
+
+@Controller
+public class HealthcheckApiController implements HealthcheckApi {
+
+    private static final Logger log = LoggerFactory.getLogger(HealthcheckApiController.class);
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public HealthcheckApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    public ResponseEntity<Healthcheck> healthcheckUserGet() {
+        String accept = request.getHeader("Accept");
+        if (accept != null) {
+            try {
+                return new ResponseEntity<Healthcheck>(objectMapper.readValue("{  \"message\" : \"healthcheck\",  \"status\" : \"healthy\"}", Healthcheck.class), HttpStatus.OK);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<Healthcheck>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<Healthcheck>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+}
