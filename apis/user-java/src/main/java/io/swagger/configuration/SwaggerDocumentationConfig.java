4aef2558634854f0c777a695203991f053eebd65
Aparna <aprao@Aparnas-MBP.guest.corp.microsoft.com>
2018-08-14 14:52:45 -0700

    Fix docs UI endpoint

--- apis/user-java/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ apis/user-java/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -32,6 +33,7 @@ public class SwaggerDocumentationConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                     .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
+                    .paths(PathSelectors.any())
                     .build()
                 .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
