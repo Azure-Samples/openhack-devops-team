bb7b60e772bd4bd9d469131358166d8bff83ef6a
apraovjr <aprao@microsoft.com>
2018-08-14 15:41:43 -0700

    Update service path for api user-java (#39)
    * Update service path to user-java
    * Fix docs UI endpoint

--- apis/user-java/src/main/java/io/swagger/configuration/HomeController.java
+++ apis/user-java/src/main/java/io/swagger/configuration/HomeController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/docs/user-java")
     public String index() {
         System.out.println("swagger-ui.html");
-        return "redirect:swagger-ui.html";
+        return "redirect:/swagger-ui.html";
     }
 }
