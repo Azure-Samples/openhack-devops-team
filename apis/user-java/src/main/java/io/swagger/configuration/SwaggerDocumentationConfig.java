87e1d82a26702fe27f230a4b4dbd1f3570d2aa39
apraovjr <aprao@microsoft.com>
2018-08-16 14:26:20 -0700

    Update user-java api swagger endpoint (#42)

--- apis/user-java/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ apis/user-java/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -3,6 +3,10 @@ package io.swagger.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,16 +14,19 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-08-03T19:26:46.543Z")
 
 @Configuration
-public class SwaggerDocumentationConfig {
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerDocumentationConfig extends WebMvcConfigurerAdapter {
 
     ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("My Driving User Profile API")
-            .description("API for the user profile in the My Driving example app. https://github.com/Azure-Samples/openhack-devops-team")
+            .title("My Driving User Java API")
+            .description("API for the user in the My Driving example app. https://github.com/Azure-Samples/openhack-devops-team")
             .license("")
             .licenseUrl("http://unlicense.org")
             .termsOfServiceUrl("")
@@ -35,9 +42,25 @@ public class SwaggerDocumentationConfig {
                     .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
                     .paths(PathSelectors.any())
                     .build()
+                    .pathMapping("/")
                 .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
                 .apiInfo(apiInfo());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/documentation/user-java/api-docs", "/api-docs").setKeepQueryParams(true);
+        registry.addRedirectViewController("/documentation/user-java/swagger-resources/configuration/ui","/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/documentation/user-java/swagger-resources/configuration/security","/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/documentation/user-java/swagger-resources", "/swagger-resources");
+        registry.addRedirectViewController("/documentation/user-java", "/documentation/user-java/swagger-ui.html");
+        registry.addRedirectViewController("/docs/user-java", "/documentation/user-java/swagger-ui.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/documentation/user-java/**").addResourceLocations("classpath:/META-INF/resources/");
     }
 
 }
