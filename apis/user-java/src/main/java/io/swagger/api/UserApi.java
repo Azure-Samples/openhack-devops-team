526fa7148eecae94ffa19268a6acbf1281a3f5c5
Aparna <aprao@Aparnas-MBP.guest.corp.microsoft.com>
2018-08-17 15:34:34 -0700

    Add ingress route for swagger

--- apis/user-java/src/main/java/io/swagger/api/UserApi.java
+++ apis/user-java/src/main/java/io/swagger/api/UserApi.java
@@ -10,19 +10,12 @@ import io.swagger.model.InlineResponseDefault;
 import io.swagger.model.Profile;
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
-import javax.validation.constraints.*;
-import java.util.List;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-08-03T19:26:46.543Z")
 
 @Api(value = "user", description = "the user API")
@@ -34,7 +27,6 @@ public interface UserApi {
             @ApiResponse(code = 404, message = "User profile not found"),
             @ApiResponse(code = 200, message = "Unknown Error", response = ErrorResponseDefault.class) })
     @RequestMapping(value = "/user-java/{userID}",
-            produces = { "application/json" },
             consumes = { "application/json" },
             method = RequestMethod.PATCH)
     ResponseEntity<Profile> updateUser(@ApiParam(value = "User's unique ID",required=true) @PathVariable("userID") String userID,@ApiParam(value = "Details of the profile" ,required=true )  @Valid @RequestBody Profile profile);
@@ -44,7 +36,6 @@ public interface UserApi {
             @ApiResponse(code = 201, message = "Creation successful", response = Profile.class),
             @ApiResponse(code = 200, message = "An error occurred", response = InlineResponseDefault.class) })
     @RequestMapping(value = "/user-java/{userID}",
-            produces = { "application/json" },
             consumes = { "application/json" },
             method = RequestMethod.POST)
     ResponseEntity<Profile> userPOST(@ApiParam(value = "User's unique ID",required=true) @PathVariable("userID") String userID,@ApiParam(value = "Details of the profile" ,required=true )  @Valid @RequestBody Profile profile);
