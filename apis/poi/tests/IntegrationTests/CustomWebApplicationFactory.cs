2c4752dcd9391f7dd372f2bf3904cd13ec222b29
Mikael Stadden <mikaelsnavy@gmail.com>
2018-08-30 12:39:40 -0700

    test of integration tests

--- apis/poi/tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ apis/poi/tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -30,7 +30,7 @@ namespace IntegrationTests
                     logging.AddConsole();
                     logging.AddDebug();
                 })
-                //.UseStartup<IntegrationTests.Startup>()
+                .UseStartup<IntegrationTests.Startup>()
                 .UseUrls("http://localhost:8080");
 
             return host;
