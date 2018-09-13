e6974b36edd3c0abe2454e49065dacf7bcbb13a0
Hadwa Abdelhalem <hadwaa@microsoft.com>
2018-09-13 11:51:37 -0700

    Add POI unit test

---
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using poi.Utility;
+
+namespace UnitTests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void TestLoggingEvents()
+        {
+            Assert.Equal(1000, LoggingEvents.Healthcheck);
+            Assert.Equal(2001, LoggingEvents.GetAllPOIs);
+        }
+    }
+}
