2367f1b49a8cc1a949e095cbb9a3bd795ae63e17
hadwa.abdelhalem <34171319+HadwaAbdelhalem@users.noreply.github.com>
2018-09-14 14:26:48 -0700

    Add POI unit test (#64)

---
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using poi.Utility;
+
+namespace UnitTests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void TestLoggingEvents()
+        {
+            Assert.Equal(1000, LoggingEvents.Healthcheck);
+            Assert.Equal(2001, LoggingEvents.GetAllPOIs);
+            Assert.Equal(2002, LoggingEvents.GetPOIByID);
+            Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
+        }
+    }
+}
