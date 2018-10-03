4a6fd4c04f2ff7e4fc403d1284a1e011fb155107
iigna <31695453+iiosif@users.noreply.github.com>
2018-10-03 12:54:40 +0200

    simulate build failure change msg

--- apis/userprofile/data/healthcheck/user.js
+++ apis/userprofile/data/healthcheck/user.js
@@ -15,7 +15,7 @@ module.exports = {
     get: {
         200: function (req, res, callback) {
             res.json({
-                message: 'healthcheck',
+                message: 'healthcheck-task3',
                 status: 'healthy'
             });
             callback;
