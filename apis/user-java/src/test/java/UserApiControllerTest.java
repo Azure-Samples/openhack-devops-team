0883d4bceb1d907d41b300548e38e0c34f40ba4e
U710221 <rafal.molenda@lhsystems.com>
2018-10-03 13:49:37 +0200

    run tests.

--- apis/user-java/src/test/java/UserApiControllerTest.java
+++ apis/user-java/src/test/java/UserApiControllerTest.java
@@ -70,7 +70,7 @@ public class UserApiControllerTest {
         )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
-        verify(userRepositoryService, times(7)).save(profile);
+        verify(userRepositoryService, times(1)).save(profile);
     }
 
     @Test
