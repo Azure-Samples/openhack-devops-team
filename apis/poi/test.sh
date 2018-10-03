9cdc63d327e5e89076d3a5387cfe382e0fbe17d5
Ubuntu <jenkins@jenkinsopenhackiva6.gh5somtrzrluzbfg2w3us5txnb.ax.internal.cloudapp.net>
2018-10-03 08:34:22 +0000

    unit treshold and coverage

---
+++ apis/poi/test.sh
@@ -0,0 +1,14 @@
+cd tests/UnitTests/
+dotnet restore
+dotnet build
+cd ../..
+cd tools
+dotnet minicover instrument --workdir ../ --assemblies tests/UnitTests/bin/Debug/netcoreapp2.1/*.dll --sources web/**/*.cs --sources web/*.cs
+dotnet minicover reset
+cd ..
+for project in tests/UnitTests/*.csproj; do dotnet test --no-build $project; done
+cd tools
+dotnet minicover uninstrument --workdir ../
+dotnet minicover htmlreport --workdir ../ --threshold 90
+dotnet minicover report --workdir ../ --threshold 90
+cd ..
