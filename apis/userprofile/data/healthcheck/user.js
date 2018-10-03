3603c512ec6eed9c9998e79f52bf3f9f83d266c3
iigna <31695453+iiosif@users.noreply.github.com>
2018-10-03 13:11:07 +0200

    resolve build failure msg

--- apis/userprofile/data/healthcheck/user.js
+++ apis/userprofile/data/healthcheck/user.js
@@ -15,7 +15,7 @@ module.exports = {
     get: {
         200: function (req, res, callback) {
             res.json({
-                message: 'healthcheck-task3',
+                message: 'healthcheck',
                 status: 'healthy'
             });
             callback;
