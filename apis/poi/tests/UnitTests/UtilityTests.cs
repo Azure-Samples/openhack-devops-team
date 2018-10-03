7e08dd93900be19f9b48991bf547513552b0e41b
iigna <31695453+iiosif@users.noreply.github.com>
2018-10-03 14:42:49 +0200

    test poi stages

--- apis/poi/tests/UnitTests/UtilityTests.cs
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -11,6 +11,7 @@ namespace UnitTests
             Assert.Equal(1000, LoggingEvents.Healthcheck);
             Assert.Equal(2001, LoggingEvents.GetAllPOIs);
             Assert.Equal(2002, LoggingEvents.GetPOIByID);
+            
             Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
         }
     }
