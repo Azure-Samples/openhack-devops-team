acc13f91f6a4ec9abcd8b48f97c36a8422ddfe78
Richard Guthrie <rguthriemsft@users.noreply.github.com>
2018-11-05 09:28:35 -0800

    bug in poi api constructor (#102)

--- apis/poi/web/Controllers/POIController.cs
+++ apis/poi/web/Controllers/POIController.cs
@@ -16,20 +16,6 @@ namespace poi.Controllers
         public POIController(POIContext context)
         {
             _context = context;
-
-            if (_context.POIs.Count() == 0)
-            {
-               _context.POIs.Add(new POI
-               {
-                   TripId = Guid.NewGuid().ToString(),
-                   Latitude = 0,
-                   Longitude = 0,
-                   PoiType = POIType.HardAcceleration,
-                   Timestamp = DateTime.Now,
-                   Deleted = false
-               });
-               _context.SaveChanges();
-            }
         }
 
         [HttpGet(Name = "GetAllPOIs")]
