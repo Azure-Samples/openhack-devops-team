6f1b9cbaf5e1a866e5513fc7baf05d99ca1d70ec
Brian Sherwin <brian.sherwin@microsoft.com>
2018-12-01 13:37:04 -0500

    changed health check

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Health check";
+            Status = "Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
