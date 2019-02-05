e141879cf182f2408ff35ab8a3dadb34b55244e7
Sven Malvik <sven.malvik@vipps.no>
2019-02-05 15:05:13 +0100

    Error3

--- apis/user-java/src/test/java/UserApiControllerTest.java
+++ apis/user-java/src/test/java/UserApiControllerTest.java
@@ -69,7 +69,7 @@ public class UserApiControllerTest {
                         .content(convertObjectToJsonBytes(profile))
         )
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType("BLA5"));
+                .andExpect(content().contentType("BLA6"));
         verify(userRepositoryService, times(1)).save(profile);
     }
 
