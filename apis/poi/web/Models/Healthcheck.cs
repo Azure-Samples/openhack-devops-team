cfdc155b6adbb3531e550d66d4245dbb9c247d33
Nick Ward <nickward13@hotmail.com>
2019-11-11 11:51:50 +1100

    changed health check message

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -7,7 +7,7 @@ namespace poi.Models
     {
         public Healthcheck()
         {
-            Message = "POI Service Healthcheck";
+            Message = "POI Service Healthchecks";
             Status = "Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
