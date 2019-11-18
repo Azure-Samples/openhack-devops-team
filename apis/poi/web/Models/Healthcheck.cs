f4a37bb0945dd1391ca642ac37c00c37e1c600c6
Rohit Phansalkar <Rohit.Phansalkar@hollardcommercial.com.au>
2019-11-18 14:07:24 +1100

    making the test to fail

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -7,7 +7,7 @@ namespace poi.Models
     {
         public Healthcheck()
         {
-            Message = "POI Service Healthcheck";
+            Message = "POI Service Healthcheck wrong message";
             Status = "Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
