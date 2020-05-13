e011dddf5a67c1a582e8dd7240d39a5fbf452006
cocallaw <coreylcallaway@gmail.com>
2020-05-13 16:08:12 -0400

    Update dotnetcore.yml

--- apis/trips/tests/healthroute_test.go
+++ apis/trips/tests/healthroute_test.go
@@ -13,7 +13,7 @@ var healthRouteTests = []tripSvc.APITestCase{
 		Tag:              "t0 - healthcheck",
 		Method:           "GET",
 		URL:              "/api/healthcheck/trips",
-		Status:           404,
+		Status:           200,
 		ExpectedResponse: `{"message": "Trip Service Healthcheck","status": "Healthy"}`,
 	},
 }
