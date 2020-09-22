aec97704629ceeb06010d4ed197143e496f11c61
hiroyay-ms <hiroyay@microsoft.com>
2020-09-22 14:51:41 +0900

    Update HealthCheckTests.cs

--- apis/poi/tests/UnitTests/HealthCheckTests.cs
+++ apis/poi/tests/UnitTests/HealthCheckTests.cs
@@ -10,7 +10,7 @@ namespace UnitTests
         public void HealthCheckTestModel()
         {
             Assert.Equal("POI Service Healthcheck", new Healthcheck().Message);
-            Assert.Equal("Healthy0922", new Healthcheck().Status);
+            Assert.Equal("Healthy09221451", new Healthcheck().Status);
 
         }
     }
