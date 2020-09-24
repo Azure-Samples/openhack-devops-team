f42269962c33fbefcd5b3238b268cacc6451eba7
Jose Hui <chhui@microsoft.com>
2020-09-24 12:13:19 +0800

    add bash script

---
+++ resource/polling.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+declare -i duration=1
+declare hasUrl=""
+declare endpoint
+
+usage() {
+    cat <<END
+    polling.sh [-i] [-h] endpoint
+    
+    Report the health status of the endpoint
+    -i: include Uri for the format
+    -h: help
+END
+}
+
+while getopts "ih" opt; do 
+  case $opt in 
+    i)
+      hasUrl=true
+      ;;
+    h) 
+      usage
+      exit 0
+      ;;
+    \?)
+     echo "Unknown option: -${OPTARG}" >&2
+     exit 1
+     ;;
+  esac
+done
+
+shift $((OPTIND -1))
+
+if [[ $1 ]]; then
+  endpoint=$1
+else
+  echo "Please specify the endpoint."
+  usage
+  exit 1 
+fi 
+
+
+healthcheck() {
+    declare url=$1
+    result=$(curl -i $url 2>/dev/null | grep HTTP/1.1)
+    echo $result
+}
+
+while [[ true ]]; do
+   result=`healthcheck $endpoint` 
+   declare status
+   if [[ -z $result ]]; then 
+      status="N/A"
+   else
+      status=${result:9:3}
+   fi 
+   timestamp=$(date "+%Y%m%d-%H%M%S")
+   if [[ -z $hasUrl ]]; then
+     echo "$timestamp | $status "
+   else
+     echo "$timestamp | $status | $endpoint " 
+   fi 
+   sleep $duration
+done
