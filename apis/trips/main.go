2a5a35e413adad503b14c99ee593cde68555051d
melzayet <maelzaye@microsoft.com>
2021-01-05 14:00:04 +0000

    change 4 in trips

--- apis/trips/main.go
+++ apis/trips/main.go
@@ -33,7 +33,7 @@ func main() {
 		sw.InitLogging(os.Stdout, ioutil.Discard, os.Stdout)
 	}
 
-	sw.Info.Println(fmt.Sprintf("%s%s", "Trips Service Server started for sure on port ", *webServerPort))
+	sw.Info.Println(fmt.Sprintf("%s%s", "Trips Service Server started on port ", *webServerPort))
 
 	router := sw.NewRouter()
 
