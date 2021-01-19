60a4f9461d563b96de07e36cccc361b595b53108
sagar027 <sagar027@yahoo.com>
2021-01-19 21:47:50 +0530

    changed startup.cs

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -28,6 +28,7 @@ namespace poi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //adding comments
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
