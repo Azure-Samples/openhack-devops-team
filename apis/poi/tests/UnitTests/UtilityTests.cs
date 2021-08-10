bbfdc3ca6d2b9a6daa8e9f0de9e0e2335bb41f78
Joel Stidley <30506169+joelst@users.noreply.github.com>
2021-08-10 18:54:41 -0500

    Update UtilityTests.cs
    Add failing test of 1 = 2

--- apis/poi/tests/UnitTests/UtilityTests.cs
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -12,6 +12,7 @@ namespace UnitTests
             Assert.Equal(2001, LoggingEvents.GetAllPOIs);
             Assert.Equal(2002, LoggingEvents.GetPOIByID);
             Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
+            Assert.Equal(1, 2);
         }
     }
 }
