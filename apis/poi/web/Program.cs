15784fb35c7e187f178cabe832e3780a20b9aaff
gbeaud <79973892+gbeaud@users.noreply.github.com>
2021-09-15 10:34:54 +0200

    Added Failure in code

--- apis/poi/web/Program.cs
+++ apis/poi/web/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using poi.Utility;
+FAILURE!!!
+
 namespace poi
 {
     public class Program
@@ -16,7 +18,7 @@ namespace poi
         {
 
             BuildWebHost(args).Run();
-
+            FAILURE!!!
         }
 
         public static IWebHost BuildWebHost(string[] args) {
