a3f1fce863c41aab0ca0ba376bf27680f7a77753
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2021-10-03 14:02:39 +0200

    deploy bicep

--- bicep/deploy.sh
+++ bicep/deploy.sh
@@ -53,21 +53,17 @@ elif ! [ -x "$(command -v pwsh)" ]; then
     exit 1
 fi
 
-_parse_azuresp_json() {
+azure_login() {
     _azuresp_json=$(cat azuresp.json)
     export ARM_CLIENT_ID=$(echo "${_azuresp_json}" | jq -r ".clientId")
     export ARM_CLIENT_SECRET=$(echo "${_azuresp_json}" | jq -r ".clientSecret")
     export ARM_SUBSCRIPTION_ID=$(echo "${_azuresp_json}" | jq -r ".subscriptionId")
     export ARM_TENANT_ID=$(echo "${_azuresp_json}" | jq -r ".tenantId")
-}
-
-_azure_login() {
-    _parse_azuresp_json
     az login --service-principal --username "${ARM_CLIENT_ID}" --password "${ARM_CLIENT_SECRET}" --tenant "${ARM_TENANT_ID}"
     az account set --subscription "${ARM_SUBSCRIPTION_ID}"
 }
 
-_azure_logout() {
+azure_logout() {
     az logout
     az cache purge
     az account clear
@@ -79,7 +75,6 @@ lint_bicep(){
 }
 
 validate_bicep(){
-    _azure_login
     if [ ${#RESOURCES_PREFIX} -gt 0 ]; then
         az deployment sub validate --name "${BUILD_ID}" --template-file main.bicep --location "${LOCATION}" --parameters resourcesPrefix="${RESOURCES_PREFIX}"
     elif [[ ${#RESOURCES_PREFIX} -eq 0 && ${#UNIQUER} -gt 0 ]]; then
@@ -87,11 +82,9 @@ validate_bicep(){
     else
         az deployment sub validate --name "${BUILD_ID}" --template-file main.bicep --location "${LOCATION}"
     fi
-    _azure_logout
 }
 
 preview_bicep(){
-    _azure_login
     if [ ${#RESOURCES_PREFIX} -gt 0 ]; then
         az deployment sub what-if --name "${BUILD_ID}" --template-file main.bicep --location "${LOCATION}" --parameters resourcesPrefix="${RESOURCES_PREFIX}"
     elif [[ ${#RESOURCES_PREFIX} -eq 0 && ${#UNIQUER} -gt 0 ]]; then
@@ -99,39 +92,35 @@ preview_bicep(){
     else
         az deployment sub what-if --name "${BUILD_ID}" --template-file main.bicep --location "${LOCATION}"
     fi
-    _azure_logout
 }
 
 deploy_bicep(){
-    _azure_login
     if [ ${#RESOURCES_PREFIX} -gt 0 ]; then
-        echo "If RESOURCES_PREFIX is set, then UNIQUER is ignored."
-        echo "Deploying with RESOURCES_PREFIX: ${RESOURCES_PREFIX}"
         _deployment_output=$(az deployment sub create --name "${BUILD_ID}" --template-file main.bicep --location "${LOCATION}" --parameters resourcesPrefix="${RESOURCES_PREFIX}")
     elif [[ ${#RESOURCES_PREFIX} -eq 0 && ${#UNIQUER} -gt 0 ]]; then
-        echo "Deploying with UNIQUER: ${UNIQUER}"
         _deployment_output=$(az deployment sub create --name "${BUILD_ID}" --template-file main.bicep --location "${LOCATION}" --parameters uniquer=${UNIQUER})
     else
-        echo "Deploying with LOCATION only: ${LOCATION}"
         _deployment_output=$(az deployment sub create --name "${BUILD_ID}" --template-file main.bicep --location "${LOCATION}")
     fi
-    _azure_logout
-
+    
     echo "${_deployment_output}"
 }
 
-smoke_test(){
+test_bicep(){
     local _hostnames="${1}"
-
+    
     pwsh -Command ./smokeTest.ps1 -HostNames "${hostnames}"
 }
 
 export BUILD_ID="${RANDOM:0:4}"
 
+azure_login
+
 lint_bicep
 validate_bicep
 preview_bicep
 deployment_output=$(deploy_bicep)
-echo "${deployment_output}"
 hostnames=$(echo "${deployment_output}" | jq -r -c '.properties.outputs | map(.value) | join(",")')
-smoke_test "${hostnames}"
+test_bicep "${hostnames}"
+
+azure_logout
