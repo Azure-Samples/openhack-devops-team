7bf91db86055d76d2e23427e089c7aa032fbf892
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2021-10-17 13:19:21 +0200

    poi tests (#330)
    Co-authored-by: Dariusz Porowski <Dariusz.Porowski@microsoft.com>

--- apis/poi/buildtest.sh
+++ apis/poi/buildtest.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+# clean the output of the previous build
 dotnet clean
-dotnet restore .
-dotnet build .
-dotnet test --filter "FullyQualifiedName~UnitTest"
+
+# restore dependencies
+dotnet restore
+
+# build the project
+dotnet build --no-restore
+
+# run selective test - unit tests
+dotnet test --no-build --filter "FullyQualifiedName~UnitTest"
+
+# run selective test - integrations tests
+dotnet test --no-build --filter "FullyQualifiedName~IntegrationTests"
+
+# run all tests
+dotnet test --no-build
