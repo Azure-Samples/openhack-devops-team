04ecd490fb7a41c3285edc502f899b8c8d313430
Dariusz Porowski <Dariusz.Porowski@microsoft.com>
2021-10-21 14:49:39 +0200

    userjava

--- apis/user-java/buildtest.sh
+++ apis/user-java/buildtest.sh
@@ -1,1 +1,15 @@
-mvn clean compile test package
+#!/bin/bash
+
+# https://maven.apache.org/guides/introduction/introduction-to-the-lifecycle.html
+
+# clean the output of the previous build
+mvn clean
+
+# run unit tests
+mvn test
+
+# create distributable package
+mvn package
+
+# run integration tests
+mvn verify
